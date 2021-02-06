9eba004e358b4059dd98a034d1c3603ebbb038cb
Daan Vos de Wael <daan@vosdewael.com>
2021-02-07 00:00:17 +0100

    Add `postcss` plugins + config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,18 +40,25 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="postcss" scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
+
+  & li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+p {
+  font-size: 18px;
 }
+
 a {
   color: #42b983;
 }
